c2aca8578df63ae5ae2bd7771a7aefb03bf7c120
sandeepk275 <sandeep.kumar41991@gmail.com>
2021-12-13 10:38:50 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import Header from "./Header";
 import './App.css';
+
+let subscribers = [
+  {
+    id:
+      1
+    ,
+    name: "Shilpa Bhat"
+    ,
+    phone: "8888888888"
+  },
+  {
+    id:
+      2
+    ,
+    name: "Srishti Gupta"
+    ,
+    phone: "9999999999"
+  },
+  {
+    id:3,
+    name: "sandeep kumar",
+    phone: "8076806032"
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+    <button id="button">ADD</button><br/>
+    <div id="heading-grid">
+        <span id="name">NAME</span>
+        <span id="phone">PHONE</span> 
+    </div>
+
+      {
+        subscribers.map(sub => {
+          return <div key={sub.id} className="grid-container">
+            <span className="grid-name-item">{sub.name}</span>
+            <span className="grid-phone-item">{sub.phone}</span>
+            <span><button className="dlt-btn">DELETE</button></span>
+            
+          </div>
+        })
+      }
+
     </div>
   );
 }
+
+
+
+
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Header.css';
+const Header=function (){
+    return(
+        <div className="header">Phone Directory</div>
+    )
+}
+export default Header;
